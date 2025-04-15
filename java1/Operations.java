3f7bee2c336379a05d3dbf47c02d55e9be6e391d
Abhishek-S-261 <abhisheksshettar@gmail.com>
2025-04-15 10:13:26 +0530

    Created

---
+++ java1/Operations.java
@@ -0,0 +1,27 @@
+package java1;
+
+public class Operations {
+    static double x = 10;
+    static double y = 20;
+
+    static double sum = x + y;
+    static double average = (x + y) / 2;
+
+    
+    static double Minimum() 
+    {
+        return Math.min(x, y);
+    }
+
+    static double Maximum() 
+    {
+        return Math.max(x, y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum is " + sum);
+        System.out.println("Average is " + average);
+        System.out.println("Minimum is " + Minimum());
+        System.out.println("Maximum is " + Maximum());
+    }
+}
